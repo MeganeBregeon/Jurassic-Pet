fe4a428939c8dfce64bc6bc930191ed757efb38c
Centis Menant <9centis@gmail.com>
2013-12-17 12:39:42 +0100

    update progress bar (THIS ROCKS

--- Barre de vie/script.js
+++ Barre de vie/script.js
@@ -1,112 +1,55 @@
-var barEnergie = document.getElementById('BarEnergy');
-    barHungry = document.getElementById('BarHungry');
-    wellfare=0;
-    xp=0;
-    values = 
-    {
-      faim:50,
-      energie:50
-    };
-setInterval(function()
-{
-  // Energy 
-  barEnergie.value=values.energie;
-  values.energie-=0.1;
-  if(values.energie <= 0) values.energie = 0;
+window.onload = function() {
 
-  // Food
-  barHungry.value=values.faim;
-  values.faim-=0.1;
-  if(values.faim <= 0) values.faim = 0;
+  var barFatigue  = document.getElementById('BarTired'),
+      barHungry   = document.getElementById('BarHungry'),
+      wellfare    = 0,
+      lvl         = 1,
+      values      = { faim:50, fatigue:50 };
 
-  // Bien être
-  wellfare = (values.faim + values.energie)/2;
-  document.getElementById('wellfare').innerHTML = parseInt(wellfare)+"%";
-}
-,300);
+  setInterval(function() {
+    // Energy
+    barFatigue.value=values.fatigue;
+    values.fatigue-=0.1;
+    if(values.fatigue <= 0) values.fatigue = 0;
 
-//Faire manger le dino
-document.getElementById('nourriture').onclick=function()
-{
-  values.faim+=30;
-}
-//Prendre un douche
-document.getElementById('douche').onclick=function()
-{
-  values.energie+=20;
-}
-//Dormir
-document.getElementById('dormir').onclick=function()
-{
-  values.energie+=30;
-}
-//experience
-console.log(xp);
-{
-  if (wellfare > 100)
-    xp++;
-    document.getElementById('level-number').innerHTML = xp;
-    console.log(xp);
-}
+    // Food
+    barHungry.value=values.faim;
+    values.faim-=0.1;
+    if(values.faim <= 0) values.faim = 0;
 
-
-/*function progressBarEnergie(compteur) 
-  {
-    bar.value = compteur;
-    compteur--;
-
-    document.getElementById('sport').onclick=function()
-    {
-      faitdusport=true;
-      //bar.value - 40;
-      //compteur--;
-      //var amountLoaded = 100 - bar.value;
-      //progressBarEnergie(amountLoaded);
-      //alert('test')
+    // Bien être
+    wellfare = (values.faim + values.fatigue)/2;
+    if(wellfare >= 100) {
+      document.getElementById('wellfare').innerHTML = 100 + "%";
+    } else {
+      document.getElementById('wellfare').innerHTML = parseInt(wellfare)+"%";
     }
 
-    if(faitdusport==true){
-      compteur-=40;
-      console.log(compteur);
+    if (wellfare >= 100) {
+      values.fatigue = 50;
+      values.faim = 50;
+      lvl++;
+      document.getElementById('level-number').innerHTML = lvl;
     }
+  }, 300);
 
-    values.energie=compteur;
-
-    var timer = setTimeout("progressBarEnergie("+compteur+")",1000);
-    
-    if(compteur == 0)
-    {
-      bar.value = 0;
-      clearTimeout(timer);
-    }
-  }
-var amountLoaded = 100;
-progressBarEnergie(amountLoaded);
-function progressbarHungry(compteurFaim) 
+  //Faire manger le dino
+  document.getElementById('nourriture').onclick=function()
   {
-    barHungry.value = compteurFaim;
-    compteurFaim--;
-    values.faim=compteurFaim;
-    var timerFaim = setTimeout("progressbarHungry("+compteurFaim+")",700);
-      if(compteurFaim == 0)
-        {
-          barHungry.value = 0;
-          clearTimeout(timerFaim);
-        }
+    values.faim = values.faim + (50 / lvl);
   }
-var amountLoadedFaim = 100;
-progressbarHungry(amountLoadedFaim);
-
-function Bonheur()
+  //Prendre un douche
+  document.getElementById('douche').onclick=function()
   {
-    var wellfare = (values.faim+values.energie)/2;
-    bar_wellfare.value = wellfare;
-    var timerBonheur = setTimeout("Bonheur("+wellfare+")",1);
-    document.getElementById('bla').innerHTML = parseInt(wellfare)+"%";
+    values.fatigue = values.fatigue + (50 / lvl);
   }
-Bonheur();
-*/
+  //Dormir
+  document.getElementById('dormir').onclick=function()
+  {
+    values.fatigue = values.fatigue + (100 / lvl);
+  }
 
+};
 
 
 
